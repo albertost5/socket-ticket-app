5e4e8925118e735b01f833a48b899fc21c880664
albertost5 <sancheztorresalberto@gmail.com>
2022-06-18 19:19:47 +0200

    Added new ticket event

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,3 +1,30 @@
-
-
 console.log('New ticket HTML');
+
+// HTML references
+const LBL_NEW_TICKET = document.querySelector('#lblNewTicket');
+const BTN_CREATE = document.querySelector('button');
+
+const socket = io();
+
+
+socket.on('connect', () => {
+    BTN_CREATE.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    BTN_CREATE.disabled = true;
+});
+
+socket.on('last-ticket', lastTicketNumber => {
+    LBL_NEW_TICKET.innerHTML = `Ticket: ${ lastTicketNumber }`;
+});
+
+
+// socket.emit
+BTN_CREATE.addEventListener( 'click', () => {
+
+    socket.emit('next-ticket', null, ( ticket ) => {
+        LBL_NEW_TICKET.innerHTML = ticket;
+    });
+
+});
--- src/api/sockets/socket.controller.js
+++ src/api/sockets/socket.controller.js
@@ -6,12 +6,24 @@ const socketController = socket => {
 
     console.log(`Client ${ socket.id } connected..`);
 
+    // Last ticket event
+    socket.emit( 'last-ticket', ticketControl.last );
+    
+    socket.on( 'connection', () => {
+        
+    });
+
     socket.on( 'disconnect', () => {
        
     });
 
-    socket.on( 'send-message', ( payload ) => {
+    socket.on( 'next-ticket', ( payload, callback ) => {
        
+        // Return string: Ticket: 10.
+        const next = ticketControl.nextTicket();
+        callback( next )
+
+        // TO DO: There is a new ticket to be attended.
     });
 }
 
